8f3065df9190abb2a22b4df1ff2e14ddbd6ba1c3
Alejandro Navarro <147422533+AleNaGa@users.noreply.github.com>
2024-10-16 17:04:49 +0200

    proyecto creado

---
+++ src/test/java/com/vedruna/CrudProducts/CrudProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedruna.CrudProducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
